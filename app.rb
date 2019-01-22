09c4a4f589264f279eb55475f86dc06356dafb33
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-22 14:20:33 +0300

    editing

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require 'bundler'
+require 'json'
+require 'csv'
 Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'app/scrapper'
-
 # call the scrapp_data and save_as_JSON
-scrapp_data
-save_as_JSON(scrapp_data)
+@tabs = Scrapper.new
+def save_as_JSON
+	File.open("/home/malala/Documents/THP/suite-nokogiri/db/emails.json","w") do |f|
+		f.write(JSON.pretty_generate(@tabs.perform))
+    end
+end
+# create the spreadsheet methods
+def save_as_spreadsheet
+
+end
+# create of the save of the csv
+def save_as_csv
+	CSV.open("/home/malala/Documents/THP/suite-nokogiri/db/emails.csv", "w")  do |c|
+	end
+end
+save_as_JSON
